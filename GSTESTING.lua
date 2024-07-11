54119f24e5493dc05300a46ad51f1787e5b4daad
curse <77826401+movingfeds@users.noreply.github.com>
2024-07-11 17:18:29 +0300

    Update GSTESTING.lua

--- GSTESTING.lua
+++ GSTESTING.lua
@@ -1,22 +1,3 @@
---[[
-    To Do:
-
-    [+] Notifications
-    [+] Finish Documentation (Everything)
-    
-    @gs.cc
-]]
-if not LPH_OBFUSCATED then
-    LPH_JIT = function(...) return ... end
-    LPH_JIT_MAX = function(...) return ... end
-    LPH_JIT_ULTRA = function(...) return ... end
-    LPH_NO_VIRTUALIZE = function(...) return ... end
-    LPH_NO_UPVALUES = function(f) return(function(...) return f(...) end) end
-    LPH_ENCSTR = function(...) return ... end
-    LPH_ENCNUM = function(...) return ... end
-    LPH_CRASH = function() return print(debug.traceback()) end
-end;
-
 local library = {};
 do 
     LPH_NO_VIRTUALIZE(function()
